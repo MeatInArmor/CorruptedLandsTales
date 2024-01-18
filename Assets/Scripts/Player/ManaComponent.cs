84283cba992429dce6fb24c14520a7a7b3eb6323
MeatInArmor <reinor.1st@gmail.com>
2024-01-18 19:49:42 +0300

    mpbar added, special button fix
    Добавлен рабочий манабар
    кнопки атаки и умений тратящих ману разделены на разные префабы

--- Assets/Scripts/Player/ManaComponent.cs
+++ Assets/Scripts/Player/ManaComponent.cs
@@ -9,9 +9,21 @@ namespace ShadowChimera
     public class ManaComponent : MonoBehaviour
     {
         [SerializeField] private float m_manaPool;
-        
+        [SerializeField] private float m_currentMana = 0;
         public float CurrentMana => m_currentMana;
-        private float m_currentMana = 0;
+        public float manaPercent => m_currentMana / m_manaPool;
+
+        public event System.Action<float> onSpendMana;
+        public event System.Action<float> onGainMana;
+
+        public void Initialize(float max, float initMp)
+        {
+            m_manaPool = max;
+            m_currentMana = initMp;
+            onSpendMana?.Invoke(0);
+            onGainMana?.Invoke(0);
+
+        }
         public bool SpendMana(float manaCost)
         {
             if (manaCost <= m_currentMana)
@@ -19,11 +31,13 @@ namespace ShadowChimera
                 manaCost = Mathf.Min(manaCost, m_currentMana);
                 m_currentMana -= manaCost;
                 Debug.Log($"Spend {manaCost} mana, current mana is {m_currentMana}");
+                onSpendMana?.Invoke(manaCost);
                 return true;
             }
             else
             {
                 Debug.Log($"Try to Spend {manaCost} mana, current mana is {m_currentMana}");
+                onSpendMana?.Invoke(manaCost);
                 return false;
             }
         }
@@ -32,6 +46,8 @@ namespace ShadowChimera
         {
             m_currentMana = manaCount + m_currentMana > m_manaPool ? (m_manaPool) : manaCount + m_currentMana;
             Debug.Log($"gain {manaCount} mana, manapool is {m_manaPool}, current mana is {m_currentMana}");
+            onGainMana?.Invoke(manaCount);
+
         }
     }
 }
