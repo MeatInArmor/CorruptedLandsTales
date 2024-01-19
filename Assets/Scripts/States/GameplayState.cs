f439116f4469c93e9118fc41d5f31a72220627a0
MeatInArmor <reinor.1st@gmail.com>
2024-01-19 03:00:14 +0300

    ui fix, gameover panel added

--- Assets/Scripts/States/GameplayState.cs
+++ Assets/Scripts/States/GameplayState.cs
@@ -1,6 +1,7 @@
 using CorruptedLandTales;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 namespace CorruptedLandTales
@@ -11,18 +12,23 @@ namespace CorruptedLandTales
         public GameWinState gameWinState;
         public PauseState pauseState;
         public MapState mapState;
-        
+        public HealthComponent m_healthComponent;
+
+
         protected override void OnEnable()
         {
+            m_healthComponent.onDie += GameOver;
             base.OnEnable();
         }
         protected override void OnDisable()
         {
+            m_healthComponent.onDie -= GameOver;
             base.OnDisable();
         }
         public void GameOver()
         {
             Exit();
+            mapState.Exit();
             gameOverState.Enter();
         }
         //private void OnGameWin()
